d172a80fe2ecdffb12146f48f0c15dd5f6aaa707
gogallor <109631890+gogallor@users.noreply.github.com>
2023-01-24 08:44:15 +0400

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -4,12 +4,18 @@
 # In[ ]:
 
 
-n, m = map(int, input().split())
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('n', type=int)
+parser.add_argument('m', type=int)
+args = parser.parse_args()
+
+#n, m = map(int, input().split())
 
 i = 1
 while True:
     print(i, end='')
-    i = 1 + (i + m - 2) % n
+    i = 1 + (i + args.m - 2) % args.n
     if i == 1:
         break
 print()
